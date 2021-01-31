2b21f659e3e92e297657b3945145d5b2c7a9df54
RakeshKumarA <rkakumar85@gmail.com>
2021-01-31 18:51:07 +0530

    Changes for git

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
-import App from './App';
-import theme from './theme';
-import store from './app/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import App from "./App";
+import theme from "./theme";
+
+// Redux related components and libraries
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,5 +14,5 @@ ReactDOM.render(
       <App />
     </ThemeProvider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
